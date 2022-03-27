b2d2ac80ab037ba6daad3fe6f0f9ff5f5e4cbfa6
svistopljaska <svistopljaska@yandex.ru>
2022-03-27 12:40:29 +0500

    changed listener's port to :80

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	r := api.InitRouter()
 	srv := &http.Server{
 		Handler:      r,
-		Addr:         "127.0.0.1:8000",
+		Addr:         "127.0.0.1:80",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
